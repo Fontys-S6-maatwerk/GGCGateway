3c3999df25b91cf4efc9d0d38d63214c6c1f1b61
Tim Gooren <384376@student.fontys.nl>
2021-10-07 11:31:57 +0200

    Fix code smell

--- GGCGateway/Program.cs
+++ GGCGateway/Program.cs
@@ -12,7 +12,7 @@ using Ocelot.DependencyInjection;
 
 namespace GGCGateway
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
